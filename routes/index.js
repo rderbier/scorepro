51cf7e764564eb05a693f594048b57119712f48a
Raphael Derbier <rderbier@tibco.com>
2018-03-11 19:14:24 -0700

    update backend app pages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var event = require('../models/event.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Welcome' });
 });
-
+router.get('/newevent', function(req, res, next) {
+  res.render('newevent', { title: 'New Event' });
+});
+router.post('/newevent', function(req, res, next) {
+	event.saveEvent(req.body).then(
+        function(item) {
+            
+            res.render('event-details', item);
+        });
+  
+});
 module.exports = router;
